b84221f0f8482ef64439df996a2fc24bb6abe75a
jamesj <ehddnr177@uangel.com>
2021-07-02 16:53:10 +0900

    # 변경사항 1. 구조 변경 중 1) 천이 성공 시 CallBack 실행 (CallBack) 2) 천이 실패 시 timeout 후 event 실행 > Timer thread 필요

--- FSMManager/src/main/java/state/basic/module/StateHandler.java
+++ FSMManager/src/main/java/state/basic/module/StateHandler.java
@@ -54,13 +54,20 @@ public class StateHandler {
      * @fn public boolean addState (String event, String fromState, String toState, String failState, CallBack callBack)
      * @brief 새로운 State 를 추가하는 함수
      * fromState 가 toState 로 천이되기 위한 trigger 이벤트와 천이 후에 실행될 CallBack 을 정의한다.
+     *
+     * 1) 천이 성공 시 CallBack 실행 (CallBack)
+     * 2) 천이 실패 시 timeout 후 event 실행 (TaskUnit 필요)
+     *
      * @param event Trigger 될 이벤트 이름
      * @param fromState 천이 전 State 이름
      * @param toState 천이 후 State 이름
-     * @param callBack 천이 후 실행될 CallBack
+     * @param callBack 천이 성공 후 실행될 CallBack
+     * @param failEvent 천이 실패 후 실행될 이벤트 이름
+     * @param delay 천이 실패 후 실행될 이벤트가 실행되기 위한 Timeout 시간
      * @return 성공 시 true, 실패 시 false 반환
      */
-    public boolean addState (String event, String fromState, String toState, CallBack callBack) {
+    public boolean addState (String event, String fromState, String toState, CallBack callBack, String failEvent, int delay) {
+        // TODO
         return stateEventManager.addEvent(event, fromState, toState) &&
                 stateContainer.addToStateByFromState(fromState, toState, callBack);
     }
